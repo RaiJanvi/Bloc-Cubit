c8f3f971dbb7b78aaa134d17d85decad960b0234
Janvi Rai <janvirai000@gmail.com>
2022-03-28 13:58:05 +0530

    Simple Counter Cubit

---
+++ lib/cubit/counter_state.dart
@@ -0,0 +1,15 @@
+part of 'counter_cubit.dart';
+
+// abstract class CounterState extends Equatable {
+//   const CounterState();
+// }
+
+class CounterState{
+  final int counterValue;
+  final bool? wasIncremented;
+
+  const CounterState({required this.counterValue, this.wasIncremented});
+
+  @override
+  List<Object> get props => [];
+}
